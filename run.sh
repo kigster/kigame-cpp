488b6122dd0893801e4e1a5595155b08ec6a10b1
Konstantin Gredeskoul <kig@flowinc.com>
2018-11-26 13:38:50 -0800

    Do not force CLang type compiler on this project.
    This now works with GCC+-8 and CLang compilers.

--- run.sh
+++ run.sh
@@ -2,19 +2,26 @@
 
 set -e -x
 
-git submodule init 
-git submodule update 
+DCC="/usr/local/bin/gcc"
+DCXX="/usr/local/bin/g++-8"
+
+if [[ -z "${FORCE_DEFAULT_COMPILER}" && -x "${DCC}" && -x "${DCXX}" ]]; then
+  export CC=${CC:-${DCC}}
+  export CXX="${CXX:-${DCXX}}"
+fi
+
+# git submodule init && git submodule update 
 
 rm -rf build bin lib
-mkdir -p build/debug
-cd build/debug
-cmake ../..
-make -j 4 
-make install
+mkdir -p build/debug && cd build/debug
+
+cmake ../.. && make -j 8 && make install
+
 cd ../..
 
 # now run the tests
 for binary in $(find bin -type f -perm -111); do 
+  echo " Running file ${binary}..."
   ${binary}
 done
 
